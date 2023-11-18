d6b889964c1aacc7a19051034bb1fe1d369847a2
Cristian Gutierrez <cdgutierrez456@gmail.com>
2023-11-17 23:42:20 -0500

    Creating folders form proyect

---
+++ front/script.js
@@ -0,0 +1,59 @@
+const btnGetData = document.querySelector("#getData")
+const btnSaveData = document.querySelector("#saveData")
+const firstInput = document.querySelector("#firstInput")
+const secondInput = document.querySelector("#secondInput")
+
+const sectionForInfo = document.querySelector("#showInformation")
+
+const url = "http://localhost:3000/api/v1/products"
+
+btnGetData.addEventListener("click", (e) => {
+  e.preventDefault()
+  getInfo()
+});
+
+const getInfo = async () => {
+  const response = await fetch(url, {
+    method: "GET"
+  })
+  const products = await response.json()
+  setProductsHTML(products)
+}
+
+const setProductsHTML = (products) => {
+  if (products.length) {
+    console.log(products)
+    sectionForInfo.innerHTML = "";
+    products.forEach(product => {
+      const div = document.createElement("div")
+      const idDiv = document.createElement("div")
+      const nameDiv = document.createElement("div")
+      const priceDiv = document.createElement("div")
+      idDiv.textContent = product.id
+      nameDiv.textContent = product.name
+      priceDiv.textContent = product.price
+
+      div.classList.add("grid")
+      div.appendChild(idDiv)
+      div.appendChild(nameDiv)
+      div.appendChild(priceDiv)
+      sectionForInfo.appendChild(div)
+    });
+  }
+}
+
+const saveData = async (object) => {
+  const saved = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(object)
+  })
+}
+
+btnSaveData.addEventListener("click", (e) => {
+  e.preventDefault()
+  let body = {
+    product: firstInput.value,
+    lastname: secondInput.value 
+  }
+  saveData(body)
+})
